07e71da83049091d77d4350fcd1bda20ca57ca2f
dousu <hiroki.sudo.hs@gmail.com>
2018-06-24 01:50:02 +0900

    add simple program option on comment

--- SOURCE/Parameters.cpp
+++ SOURCE/Parameters.cpp
@@ -264,55 +264,199 @@ Parameters::string_join(const std::vector<std::string> & str_v, const std::strin
 }
 
 /*
-struct A{
-  int i;
-  A(){i=0};
-  A & add_int(){
-    return *this;
-  }
-  A & operator()(int j){
-    i += j;
+#include <iostream>
+#include <type_traits>
+#include <variant>
+#include <map>
+#include <iterator>
+#include <string>
+#include <sstream>
+
+#include "IndexFactory.h"
+
+struct E{
+  using F=std::variant<bool,int,double,std::string>;
+private:
+  F obj;
+public:
+  E(){};
+
+  template<typename T>
+  E(T val){
+    obj = val;
+  };
+  E & operator=(const E & other){
+    if(other.check_type<int>()){
+      obj = other.var_get<int>();
+    }else if(other.check_type<double>()){
+      obj = other.var_get<double>();
+    }else if(other.check_type<std::string>()){
+      obj = other.var_get<std::string>();
+    }else if(other.check_type<bool>(other.obj)){
+      obj = other.var_get<bool>();
+    }else{
+      std::cerr << "irregular option value" << std::endl;
+      exit(1);
+    }
     return *this;
   };
+
+  template<typename T>
+  E & operator=(const T val){
+    //型チェック
+    if(check_type<int>()){
+      if(!std::is_same<int,T>::value){
+        std::cerr << "conflict type data" << std::endl;
+      }
+    }else if(check_type<double>()){
+      if(!std::is_same<double,T>::value){
+        std::cerr << "conflict type data" << std::endl;
+      }
+    }else if(check_type<std::string>()){
+      if(!std::is_same<std::string,T>::value){
+        std::cerr << "conflict type data" << std::endl;
+      }
+    }else if(check_type<bool>()){
+      if(!std::is_same<bool,T>::value){
+        std::cerr << "conflict type data" << std::endl;
+      }
+    }else{
+      std::cerr << "irregular option value" << std::endl;
+      exit(1);
+    }
+    C value = val;
+    obj = value;
+    return *this;
+  };
+
+  template<typename T>
+  T var_get() const{
+    return std::get<T>(obj);
+  }
+
+  template<typename T>
+  bool check_type(){
+    return std::holds_alternative<T>(obj);
+  }
 };
-int main(void){
-  A a;
-  a.add_int()
-    (3)
-    (2)
-    (-1)
-    (7)
-    (5);
-  if(a.i != 16){
-    exit(1);
-  }else{
-    return 0;
+struct G{
+  IndexFactory idx;
+  std::map<std::string, int> id;
+  std::map<int, E> val_list;
+  std::map<int, std::string> desc_list;
+  enum type_id {itype, dtype, stype, btype};
+  std::map<int, type_id> type_inf;
+
+  template<typename T>
+  static T value(){return T();}
+
+  template<typename T>
+  static T value(T obj){return T(obj);}
+
+  G & add_option(){return *this;}
+
+  G & operator()(std::string key, E val, std::string str){
+    int index = idx.generate();
+    id[key] = index;
+    val_list[index] = val;
+    desc_list[index] = str;
+    if(val.check_type<int>()){
+      type_inf[index] = itype;
+    }else if(val.check_type<double>()){
+      type_inf[index] = dtype;
+    }else if(val.check_type<std::string>()){
+      type_inf[index] = stype;
+    }else if(val.check_type<bool>()){
+      type_inf[index] = btype;
+    }else{
+      std::cerr << "unknown type" << std::endl;
+    }
+    return *this;
   }
-}
-
-struct C{
-  using D = std::variant<int,double,std::string>;
-  std::vector<D> vec;
-
-  template<typename T>
-  static T value(){
-    return T();
-  };
+  G & operator()(std::string key, std::string str){
+    int index = idx.generate();
+    id[key] = index;
+    val_list[index] = bool(false);
+    desc_list[index] = str;
+    type_inf[index] = btype;
+    return *this;
+  }
 
   template<typename T>
-  static T value(T obj){
-    return T(obj);
-  };
-
-  C & add_option(){return *this;};
-  D & get(int i){return vec[i];};
-}
-int main(void){
-  A a;
-  a.add_option() (A::value<int>(5)) (A::value<double>(0.5)) (A::value<std::string>("Hello"));
-  std::cout << std::get<int>(a.get(0)) << std::endl;
-  std::cout << std::get<double>(a.get(1)) << std::endl;
-  std::cout << std::get<std::string>(a.get(2)) << std::endl;
-  return 0;
+  T get(std::string str){
+    return val_list[id[str]].var_get<T>();
+  }
+  void help(){
+    std::string help_comment = "HELP:\n";
+    for(auto index : id){
+      help_comment += "  " + id.first + "\t" + desc_list[id.second].second + "\n";
+    }
+    std::cout << help_comment << std::endl;
+    exit(0);
+  }
+  void parse(int argc, char* argv[]){
+    int i = 1;
+    std::string key = "";
+    std::stringstream ss;
+    while(i < argc){
+      std::string str(argv[i]);
+      std::string option;
+      if(str.find("-") != 0 ||
+        (str.find("-") == 0 && str.size() < 2) ||
+        (str.find("--") == 0 && str.size() < 3)){
+        if(id.count(key) == 0){
+          std::cerr << "incorrect number of options" << std::endl;
+          exit(1);
+        }else{
+          type_id ti = type_inf[key];
+          bool b;
+          switch(ti){
+          case itype:
+            val_list[id[key]] = std::stoi(str);
+            break;
+          case dtype:
+            val_list[id[key]] = std::stod(str);
+            break;
+          case stype:
+            val_list[id[key]] = str;
+            break;
+          case btype:
+            ss << std::boolalpha << str;
+            ss >> b;
+            ss << std::noboolalpha;
+            val_list[id[key]] = b;
+            break;
+          default:
+            std::cerr << "unknown type_id" << std::endl;
+            exit(1);
+          }
+        }
+        key = "";
+        i++;
+        continue;
+      }
+      if(key != ""){
+        val_list[id[key]] = true;
+      }
+      if(str.find("--") == 0){
+        std::copy(std::begin(str)+2, std::end(str), std::back_inserter(option));
+      }else{
+        std::copy(std::begin(str)+1, std::end(str), std::back_inserter(option));
+      }
+      if(option == "h" || option == "help"){
+        help();
+      }
+      if(id.count(option)){
+        key = option
+        i++;
+      }else{
+        std::cerr << "unknown option" << std::endl;
+        exit(1);
+      }
+    }
+    if(key != ""){
+      val_list[id[key]] = true;
+    }
+  }
 }
 */
